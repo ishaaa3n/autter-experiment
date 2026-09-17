4f18a6c78429c7333bb2f565fb2f7433997914c4
ishan <ishandesale2004@gmail.com>
2026-09-17 20:36:00 +0530

    add isPrime helper

--- index.js
+++ index.js
@@ -21,10 +21,19 @@ function divide(a, b) {
   return a / b;
 }
 
+function isPrime(n) {
+  if (n <= 1) return true;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
 console.log("add(2, 3) =", add(2, 3));
 console.log("subtract(5, 2) =", subtract(5, 2));
 console.log("multiply(4, 3) =", multiply(4, 3));
 console.log("divide(10, 2) =", divide(10, 2));
 console.log("modulo(10, 3) =", modulo(10, 3));
+console.log("isPrime(7) =", isPrime(7));
 
-export { add, subtract, multiply, divide, modulo };
+export { add, subtract, multiply, divide, modulo, isPrime };
